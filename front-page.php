53f670bf2f8b9dd975c2c82cfb6f8dacdc36cafd
JJ <jj@dstar1.com>
2025-06-30 15:17:22 -0400

    feat(front-page): update video section

--- front-page.php
+++ front-page.php
@@ -18,7 +18,7 @@ get_header();
           
           if ( $is_mobile ) {
             // Mobile version - 1080x1080 square format
-            echo do_shortcode( '[jj_aws_ivs_recording bucket="sosodefstreaming" key="ivs/v1/627627708382/m9QeULOT1S2b/2025/6/30/1/37/Buh4nGueQBdK/media/hls/master.m3u8" aspect_ratio="1/1" autoplay="true" loop="true" muted="true" controls="false"]' );
+            echo do_shortcode( '[jj_aws_ivs_recording bucket="sosodefstreaming" key="ivs/v1/627627708382/m9QeULOT1S2b/2025/6/30/19/12/4XHhlisHDjbw/media/hls/master.m3u8" aspect_ratio="1/1" autoplay="true" loop="true" muted="true" controls="false"]' );
           } else {
             // Desktop version - 1920x1080 widescreen format
             echo do_shortcode( '[jj_aws_ivs_recording bucket="sosodefstreaming" key="ivs/v1/627627708382/m9QeULOT1S2b/2025/6/30/1/41/4BjQZkmNHof2/media/hls/master.m3u8" aspect_ratio="16/9" autoplay="true" loop="true" muted="true" controls="false"]' );
